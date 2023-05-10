123fef4c25be049e68043e2b6174f0056db89fb3
DavGuett <soulsec@protonmail.com>
2023-05-10 11:21:26 -0300

    Síntese de texto adicionada

---
+++ SpeechSynthesis.java
@@ -0,0 +1,46 @@
+import com.microsoft.cognitiveservices.speech.*;
+import com.microsoft.cognitiveservices.speech.audio.*;
+
+import java.util.Scanner;
+import java.util.concurrent.ExecutionException;
+
+public class SpeechSynthesis {
+    // This example requires environment variables named "SPEECH_KEY" and "SPEECH_REGION"
+    private static String speechKey = System.getenv("SPEECH_KEY");
+    private static String speechRegion = System.getenv("SPEECH_REGION");
+
+    public static void main(String[] args) throws InterruptedException, ExecutionException {
+        SpeechConfig speechConfig = SpeechConfig.fromSubscription(speechKey, speechRegion);
+        AudioConfig audioConfig = AudioConfig.fromWavFileOutput("C:/Users/david_proa/Documents/JavaSpeech/file.wav");
+        speechConfig.setSpeechSynthesisLanguage("pt-BR"); 
+        speechConfig.setSpeechSynthesisVoiceName("pt-BR-AntonioNeural"); 
+
+        SpeechSynthesizer speechSynthesizer = new SpeechSynthesizer(speechConfig, audioConfig);
+
+        // Get text from the console and synthesize to the default speaker.
+        System.out.println("Digite o texto que deseja que seja falado>");
+        String text = new Scanner(System.in).nextLine();
+        if (text.isEmpty())
+        {
+            return;
+        }
+
+        SpeechSynthesisResult speechSynthesisResult = speechSynthesizer.SpeakTextAsync(text).get();
+
+        if (speechSynthesisResult.getReason() == ResultReason.SynthesizingAudioCompleted) {
+            System.out.println("Speech synthesized to speaker for text [" + text + "]");
+        }
+        else if (speechSynthesisResult.getReason() == ResultReason.Canceled) {
+            SpeechSynthesisCancellationDetails cancellation = SpeechSynthesisCancellationDetails.fromResult(speechSynthesisResult);
+            System.out.println("CANCELED: Reason=" + cancellation.getReason());
+
+            if (cancellation.getReason() == CancellationReason.Error) {
+                System.out.println("CANCELED: ErrorCode=" + cancellation.getErrorCode());
+                System.out.println("CANCELED: ErrorDetails=" + cancellation.getErrorDetails());
+                System.out.println("CANCELED: Did you set the speech resource key and region values?");
+            }
+        }
+
+        System.exit(0);
+    }
+}
